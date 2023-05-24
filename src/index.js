e6b2268c6f9f489397319046cbe37e7ff5f7c7e3
jesus armando macias benitez <jesusmb@finerioconnect.com>
2023-05-24 11:57:35 -0600

    corsOptions

--- src/index.js
+++ src/index.js
@@ -6,22 +6,8 @@ const providerRoute = require("./routes/providers");
 // settings
 const app = express();
 const port = process.env.PORT || 9000;
-const corsOptions ={
-  origin:'https://develop.d86foqya0ux00.amplifyapp.com', 
-  credentials:true,            //access-control-allow-credentials:true
-  optionSuccessStatus:200,
-  methods: "GET, POST, PUT, DELETE, OPTIONS"
-}
 
-// middlewares
-
-app.use(cors(corsOptions));
 app.use(express.json());
-
-
-
-
-
 app.use("/api", providerRoute);
 
 // routes
